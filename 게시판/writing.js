501161ade63920f487e2c1a76527ee6fb4bd4fd6
ryula <hobby519@naver.com>
2021-05-26 02:13:21 +0900

    게시글 쓰기

---
+++ 게시판/writing.js
@@ -0,0 +1,32 @@
+console.log($(".wts-btn"));
+$(".wts-btn").addEventListener("click", () => {
+  let data1 = new FormData();
+
+  console.log("12321312");
+  axios.get("/board/info", data1).then((response) => {
+    console.log(response);
+    const writer = response.data.item[0].writer; /*계정*/
+    console.log($("input[name=title]").value);
+    data1.append("sub", $("input[name=title]").value);
+    data1.append("cnt", $("textarea[name=contents]").value);
+    /*data1.append("writer", writer);*/
+    write();
+  });
+
+  console.log(data1);
+  function write() {
+    axios
+      .post("/board/write", data1)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          alert(error.response.data.message);
+        } else {
+          console.log(error);
+        }
+      });
+  }
+});
